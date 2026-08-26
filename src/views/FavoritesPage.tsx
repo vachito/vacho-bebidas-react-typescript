f4d5e6e44c61ab3c080b96138abca0cb080bcbfc
Rigoberto Xocoxic <vacho19@gmail.com>
2026-08-26 07:28:12 -0600

    se muestran los favoritos

--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,5 +1,28 @@
+import { useMemo } from "react"
+import DrinkCard from "../components/DrinkCard"
+import { useAppStore } from "../stores/useAppStore"
+
 export default function FavoritesPage() {
+  const favorites = useAppStore((state) => state.favorites)
+  const hasFavorites = useMemo(() => favorites.length, [favorites])
+
   return (
-    <h1>Favoritos</h1>
+    <>
+      <h1 className="text-4xl font-extrabold">Favoritos</h1>
+
+      {hasFavorites ? (
+        <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+          {favorites.map(drink => (
+            <DrinkCard
+              key={drink.idDrink}
+              drink={drink}
+            />
+          ))}
+        </div>
+      ) : (
+        <p className="text-center text-2xl my-5 font-semibold">Aún no hay favoritos registrados</p>
+      )
+      }
+    </>
   )
 }
